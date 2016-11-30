814144668834b9deeedc32cc7354f4fe177fbe91
Agnes <Agnes@Agness-MacBook-Pro.local>
2016-11-30 15:22:47 +0800

    added js file for adding cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,19 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+// if (cardOne === cardTwo){
+// 	alert("You found a match!");
+// }
+
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function(x){
+	for (var i=0; i<x; i++){
+		var toAdd = document.createElement('div');
+		toAdd.className = 'card';
+		gameBoard.append(toAdd);
+	}
+};
+
+createCards(4);
